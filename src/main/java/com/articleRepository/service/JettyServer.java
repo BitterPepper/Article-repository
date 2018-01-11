a2a5d2e467c85627f8774e452c717fc1c2ef783d
BitterPepper <developer_sa@ukr.net>
2018-01-11 15:03:15 +0200

    Update JettyServer.java

--- src/main/java/com/articleRepository/service/JettyServer.java
+++ src/main/java/com/articleRepository/service/JettyServer.java
@@ -3,8 +3,8 @@ package com.articleRepository.service;
 import com.articleRepository.controllers.RepositoryController;
 import com.articleRepository.dao.MemoryArticleDao;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.Configuration;
 import org.eclipse.jetty.webapp.WebAppContext;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
@@ -13,28 +13,23 @@ public class JettyServer {
 
     public static void main(String[] args) throws Exception {
 
-        ResourceConfig config = new ResourceConfig();
-        config.packages("com.sadovskyi");
-        injectDependencies(config);
-        ServletHolder servletHolder = new ServletHolder(new ServletContainer(config));
-
-        ServletHolder holderDef = new ServletHolder("default", DefaultServlet.class);
-        holderDef.setInitParameter("dirAllowed", "true");
-
         Server server = new Server(8080);
 
         WebAppContext context = new WebAppContext();
         context.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/[^/]*jstl.*\\.jar$");
 
-        org.eclipse.jetty.webapp.Configuration.ClassList classlist = org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(server);
+        Configuration.ClassList classlist = org.eclipse.jetty.webapp.Configuration.ClassList.setServerDefault(server);
         classlist.addAfter("org.eclipse.jetty.webapp.FragmentConfiguration", "org.eclipse.jetty.plus.webapp.EnvConfiguration", "org.eclipse.jetty.plus.webapp.PlusConfiguration");
         classlist.addBefore("org.eclipse.jetty.webapp.JettyWebXmlConfiguration", "org.eclipse.jetty.annotations.AnnotationConfiguration");
 
-        context.setContextPath("/");
         context.setResourceBase("src/main/webapp");
         context.setWelcomeFiles(new String[]{"index.jsp"});
+
+        ResourceConfig config = new ResourceConfig();
+        injectDependencies(config);
+
+        ServletHolder servletHolder = new ServletHolder(new ServletContainer(config));
         context.addServlet(servletHolder, "/articleservice/*");
-        context.addServlet(holderDef, "/");
         server.setHandler(context);
 
         try {
